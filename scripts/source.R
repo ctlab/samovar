891a37ede7d32565c222872e68dd403bc71490a8
dsmutin <98605724+dsmutin@users.noreply.github.com>
2024-02-13 16:46:50 +0300

    Major updates

--- scripts/source.R
+++ scripts/source.R
@@ -18,7 +18,7 @@ for (i in liblist) {
   if (!require(i, quietly = T, character.only = T))
     install.packages(i)
   library(i, character.only = T)
-  print(paste0(i, " loaded"))
+  cat("--- ", i, " loaded  ---\n")
 }
 
 #get current dir
@@ -28,9 +28,11 @@ cur_dir <- here(getSrcDirectory(unnamed))
 
 #source functions
 
-functions <- c(str_c(cur_dir, "scripts/functions/", dir(str_c(cur_dir, "scripts/functions"))), 
-               str_c(cur_dir, "scripts/plots/", dir(str_c(cur_dir, "scripts/plots"))))
+functions <- c(str_c(cur_dir, "/functions/", dir(str_c(cur_dir, "/functions"))), 
+               str_c(cur_dir, "/plots/", dir(str_c(cur_dir, "/plots"))))
 
 for (i in functions) source(i)
 
 rm(functions, liblist, i, cur_dir, unnamed)
+
+cat("---------\n\n")
