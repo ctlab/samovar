3c404516be80e494b147c9e19343bea83a94ab41
dsmutin <98605724+dsmutin@users.noreply.github.com>
2024-09-25 20:41:38 +0300

    Delete scripts directory

--- scripts/functions/samovar.R
+++
@@ -1,6 +0,0 @@
-samovar <- function(data, reps = 1, ...) {
-  data <- data %>% res_trim (...)
-  data_scaled <- data %>% res_normalize(...)
-  samovar <- build_samovar(...)
-  boil(data, data_scaled, samovar, reps = reps, ...)
-}
