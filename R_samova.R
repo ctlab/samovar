c3367462604307929bd3d35a2a22c15f898ed753
dsmutin <98605724+dsmutin@users.noreply.github.com>
2024-09-25 20:40:54 +0300

    Delete R_samova.R

--- R_samova.R
+++
@@ -1,5 +0,0 @@
-source("scripts/source.R")
-teatree <- GMrepo_type2data(test = T)[[1]] 
-teabag <- teatree %>% res_normalize(treshhold_amount = 10^(-3))
-samovar <- build_samovar(teabag, k_means = 200)
-tea <- boil(samovar)
